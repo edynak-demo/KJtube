b8d9ba9d44e2098e3f69d507a56fee1ff45eac16
edynak-demo <edynakdemo@gmail.com>
2020-07-20 11:48:12 -0400

    implemented html structure

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-Building a youtube type clone for my favorite youtuber thescariestmovieever after multiple videos getting removed by youtube for not following the narrative
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>KJtube</title>
+</head>
+<body>
+  Building a youtube type clone for my favorite youtuber thescariestmovieever after multiple videos getting removed by youtube for not following the narrative
+</body>
+</html>
