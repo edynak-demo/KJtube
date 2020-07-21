620d122b9babd56d00cafe52c374699034d46b75
edynak-demo <edynakdemo@gmail.com>
2020-07-21 12:14:32 -0400

    implemented upload button

--- includes/classes/videoDetailsFormProvider.php
+++ includes/classes/videoDetailsFormProvider.php
@@ -13,12 +13,14 @@ class videoDetailsFormProvider {
         $descriptionInput = $this->createDescriptionInput();
         $privacyInput = $this->createPrivacyInput();
         $categoriesInput = $this->createCategoriesInput();
+        $uploadButton = $this->createUploadButton();
         return "<form action='processing.php' method='POST'>
                     $fileInput
                     $titleInput
                     $descriptionInput
                     $privacyInput
                     $categoriesInput
+                    $uploadButton
                 </form>";
     }
 
@@ -70,6 +72,10 @@ class videoDetailsFormProvider {
 
         return $html;
 
+    }
+
+    private function createUploadButton() {
+        return "<button type='submit' class='btn btn-primary' name='uploadButton'>Upload</button>";
     }
 }
 ?>
