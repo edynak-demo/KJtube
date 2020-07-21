de9a0d5fe9c3e48525a4ebbbe90a8718b7e92863
edynak-demo <edynakdemo@gmail.com>
2020-07-21 10:32:03 -0400

    implemented the file input

--- upload.php
+++ upload.php
@@ -1,7 +1,18 @@
-<?php require_once("includes/header.php"); ?>
-        
+<?php 
+require_once("includes/header.php");
+require_once("includes/classes/videoDetailsFormProvider.php");
+?>
+
+
 <div class="column">
 
+<?php
+$formProvider = new videoDetailsFormProvider();
+echo $formProvider->createUploadForm();
+?>
+
+
 </div>
+
 
 <?php require_once("includes/footer.php"); ?>
