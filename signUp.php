577a30019e4c79f0013e2a3f335bff5b2e1ca1dd
edynak-demo <edynakdemo@gmail.com>
2020-07-22 15:11:32 -0400

    implemented account class

--- signUp.php
+++ signUp.php
@@ -1,6 +1,9 @@
 <?php 
 require_once("includes/config.php");
 require_once("includes/classes/formSanitizer.php");
+require_once("includes/classes/account.php");
+
+$account = new Account($con);
 
 if(isset($_POST["submitButton"])) {
     $firstName = FormSanitizer::sanitizeFormString($_POST["firstName"]);
@@ -13,6 +16,9 @@ if(isset($_POST["submitButton"])) {
 
     $password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
     $password2 = FormSanitizer::sanitizeFormPassword($_POST["password2"]);
+
+    $account->register($firstName, $lastName, $username, $email, $email2, $password, $password2);
+
 
 }
 
