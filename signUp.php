a5ce2615ec88cf6e1e23b10b017d8c135990ef7d
edynak-demo <edynakdemo@gmail.com>
2020-07-22 14:52:01 -0400

    implemented form sanitizer functions feature

--- signUp.php
+++ signUp.php
@@ -1,4 +1,23 @@
-<?php require_once("includes/config.php"); ?>
+<?php 
+require_once("includes/config.php");
+require_once("includes/classes/formSanitizer.php");
+
+if(isset($_POST["submitButton"])) {
+    $firstName = FormSanitizer::sanitizeFormString($_POST["firstName"]);
+    $lastName = FormSanitizer::sanitizeFormString($_POST["lastName"]);
+
+    $username = FormSanitizer::sanitizeFormUsername($_POST["username"]);
+
+    $email = FormSanitizer::sanitizeFormEmail($_POST["email"]);
+    $email2 = FormSanitizer::sanitizeFormEmail($_POST["email2"]);
+
+    $password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
+    $password2 = FormSanitizer::sanitizeFormPassword($_POST["password2"]);
+
+}
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
