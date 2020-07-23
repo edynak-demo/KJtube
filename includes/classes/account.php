d7cfce115de0dacdc7085161d5f4f85eed624d4c
edynak-demo <edynakdemo@gmail.com>
2020-07-23 11:18:47 -0400

    implemented password hashing using sha512

--- includes/classes/account.php
+++ includes/classes/account.php
@@ -24,7 +24,7 @@ class Account {
     }
 
     public function insertUserDetails($fn, $ln, $un, $em, $pw) {
-        return true;
+        $pw = hash("sha512", $pw);
     }
     
     private function validateFirstName($fn) {
