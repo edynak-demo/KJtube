2ba23970e9423239e2f899c44c035995cad18b1a
edynak-demo <edynakdemo@gmail.com>
2020-07-24 13:50:16 -0400

    implemented dislike functionality

--- assets/js/videoPlayerActions.js
+++ assets/js/videoPlayerActions.js
@@ -2,13 +2,13 @@ function likeVideo(button, videoId) {
   $.post("ajax/likeVideo.php", { videoId: videoId })
     .done(function (data) {
 
-      let likeButton = $(button);
-      let dislikeButton = $(button).siblings(".dislikeButton");
+      var likeButton = $(button);
+      var dislikeButton = $(button).siblings(".dislikeButton");
 
       likeButton.addClass("active");
       dislikeButton.removeClass("active");
 
-      let result = JSON.parse(data);
+      var result = JSON.parse(data);
       updateLikesValue(likeButton.find(".text"), result.likes);
       updateLikesValue(dislikeButton.find(".text"), result.dislikes);
 
@@ -24,10 +24,33 @@ function likeVideo(button, videoId) {
     });
 }
 
+function dislikeVideo(button, videoId) {
+  $.post("ajax/dislikeVideo.php", { videoId: videoId })
+    .done(function (data) {
+
+      var dislikeButton = $(button);
+      var likeButton = $(button).siblings(".likeButton");
+
+      dislikeButton.addClass("active");
+      likeButton.removeClass("active");
+
+      var result = JSON.parse(data);
+      updateLikesValue(likeButton.find(".text"), result.likes);
+      updateLikesValue(dislikeButton.find(".text"), result.dislikes);
+
+      if (result.dislikes < 0) {
+        dislikeButton.removeClass("active");
+        dislikeButton.find("img:first").attr("src", "assets/images/icons/thumb-down.png");
+      }
+      else {
+        dislikeButton.find("img:first").attr("src", "assets/images/icons/thumb-down-active.png")
+      }
+
+      likeButton.find("img:first").attr("src", "assets/images/icons/thumb-up.png");
+    });
+}
 
 function updateLikesValue(element, num) {
-  let likesCountVal = element.text() || 0;
+  var likesCountVal = element.text() || 0;
   element.text(parseInt(likesCountVal) + parseInt(num));
-
-
 }
