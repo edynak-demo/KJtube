94d331c3485610ebaf06d8a399b44fe498cdf954
edynak-demo <edynakdemo@gmail.com>
2020-07-24 13:20:02 -0400

    implemented image of active like

--- assets/js/videoPlayerActions.js
+++ assets/js/videoPlayerActions.js
@@ -11,10 +11,23 @@ function likeVideo(button, videoId) {
       let result = JSON.parse(data);
       updateLikesValue(likeButton.find(".text"), result.likes);
       updateLikesValue(dislikeButton.find(".text"), result.dislikes);
+
+      if (result.likes < 0) {
+        likeButton.removeClass("active");
+        likeButton.find("img:first").attr("src", "assets/images/icons/thumb-up.png");
+      }
+      else {
+        likeButton.find("img:first").attr("src", "assets/images/icons/thumb-up-active.png")
+      }
+
+      dislikeButton.find("img:first").attr("src", "assets/images/icons/thumb-down.png");
     });
 }
+
 
 function updateLikesValue(element, num) {
   let likesCountVal = element.text() || 0;
   element.text(parseInt(likesCountVal) + parseInt(num));
+
+
 }
