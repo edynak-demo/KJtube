bb479ac95281bfa97e5b8234b1d38320ca32fd5b
edynak-demo <edynakdemo@gmail.com>
2020-07-24 10:08:04 -0400

    implemented button provider class

--- includes/classes/videoInfoControls.php
+++ includes/classes/videoInfoControls.php
@@ -1,4 +1,5 @@
 <?php
+require_once("includes/classes/buttonProvider.php");
 class VideoInfoControls {
 
     private $video, $userLoggedInObj;
@@ -20,7 +21,7 @@ class VideoInfoControls {
     }
 
     private function createLikeButton() {
-        return "<button>Like</button>";
+        return ButtonProvider::createButton("Like", "", "", "");
     }
 
     private function createDislikeButton() {
