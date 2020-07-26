ea439cdae57aeffce0e9a6a37b59762d5c0a6b62
edynak-demo <edynakdemo@gmail.com>
2020-07-26 16:16:59 -0400

    implemented subscribe button

--- includes/classes/videoInfoSection.php
+++ includes/classes/videoInfoSection.php
@@ -42,7 +42,8 @@ class VideoInfoSection {
             $actionButton = ButtonProvider::createEditVideoButton($this->video->getId());
         }
         else {
-            $qctionButton = "";
+            $userToObject = new User($this->con, $uploadedBy);
+            $actionButton = ButtonProvider::createSubscriberButton($this->con, $userToObject, $this->userLoggedInObj);
         }
         
         return "<div class='secondaryInfo'>
