9db1a18a7652e8b830b91e481228771a26c45bd1
edynak-demo <edynakdemo@gmail.com>
2020-07-26 16:50:36 -0400

    implemented subscribe function

---
+++ assets/js/userActions.js
@@ -0,0 +1,12 @@
+function subscribe(userTo, userFrom, button) {
+
+  if (userTo == userFrom) {
+    alert("You can't subscribe to yourself");
+    return;
+  }
+
+  $.post("ajax/subscribe.php")
+    .done(function () {
+      console.log("done");
+    });
+}
