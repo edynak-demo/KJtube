0cba21b8c33618f0daa13af9f6ef2c439b39dad7
edynak-demo <edynakdemo@gmail.com>
2020-07-29 14:21:18 -0400

    implemented the comment class

--- ajax/postComment.php
+++ ajax/postComment.php
@@ -1,5 +1,7 @@
 <?php
 require_once("../includes/config.php");
+require_once("../includes/classes/user.php");
+require_once("../includes/classes/comment.php");
 
 if(isset($_POST['commentText']) && isset($_POST['postedBy']) && isset($_POST['videoId'])) {
     
@@ -12,13 +14,14 @@ if(isset($_POST['commentText']) && isset($_POST['postedBy']) && isset($_POST['vi
 
     $postedBy = $_POST['postedBy'];
     $videoId = $_POST['videoId'];
-    $responseTo = (int)$_POST['responseTo'];
+    $responseTo = $_POST['responseTo'];
     $commentText = $_POST['commentText'];
 
     $query->execute();
 
-    // Return new comment html
-
+    $userLoggedInObj = new User($con, $_SESSION["userLoggedIn"]);
+    $newComment = new Comment($con, $con->lastInsertId(), $userLoggedInObj, $videoId);
+    echo $newComment->create();
 }
 else {
     echo "One or more parameters are not passed into subscribe.php the file";
