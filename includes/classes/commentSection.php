5eb6f0280e4eb50562985477865f93d3b9cb2d17
edynak-demo <edynakdemo@gmail.com>
2020-07-29 13:06:56 -0400

    implemented the comment form

--- includes/classes/commentSection.php
+++ includes/classes/commentSection.php
@@ -22,7 +22,26 @@ class CommentSection {
         $commentAction = "postComment(this, \"$postedBy\", $videoId, null, \"comments\")";
         $commentButton = ButtonProvider::createButton("COMMENT", null, $commentAction, "postComment");
 
-   }
+          // Get comments html
+
+        return "<div class='commentSection'>
+
+                    <div class='header'>
+                        <span class='commentCount'>$numComments Comments</span>
+
+                        <div class='commentForm'>
+                            $profileButton
+                            <textarea class='commentBodyClass' placeholder='Add a public comment'></textarea>
+                            $commentButton
+                        </div>
+                    </div>
+
+                    <div class='comments'>
+
+                    </div>
+
+                </div>";
+    }
 
 }
-?>   
+?>
