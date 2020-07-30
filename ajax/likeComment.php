b558fa48716881716951023060d43d3fc87e6caa
edynak-demo <edynakdemo@gmail.com>
2020-07-30 12:03:21 -0400

    implemented like/dislike ajax request

---
+++ ajax/likeComment.php
@@ -0,0 +1,14 @@
+<?php
+require_once("../includes/config.php"); 
+require_once("../includes/classes/comment.php"); 
+require_once("../includes/classes/user.php"); 
+
+$username = $_SESSION["userLoggedIn"];
+$videoId = $_POST["videoId"];
+$commentId = $_POST["commentId"];
+
+$userLoggedInObj = new User($con, $username);
+$comment = new Comment($con, $commentId, $userLoggedInObj, $videoId);
+
+echo $comment->like();
+?>
