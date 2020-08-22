7e569a29954a448637945997f9c4e4c6cd0a2e88
edynak-demo <edynakdemo@gmail.com>
2020-08-22 15:21:35 -0400

    implemented display search results output

--- search.php
+++ search.php
@@ -2,7 +2,6 @@
 require_once("includes/header.php");
 require_once("includes/classes/searchResultsProvider.php");
 
-
 if(!isset($_GET["term"]) || $_GET["term"] == "") {
     echo "You must enter a search term";
     exit();
@@ -16,8 +15,26 @@ if(!isset($_GET["orderBy"]) || $_GET["orderBy"] == "views") {
 else {
     $orderBy = "uploadDate";
 }
-?>
 
+$searchResultsProvider = new SearchResultsProvider($con, $userLoggedInObj);
+$videos = $searchResultsProvider->getVideos($term, $orderBy);
+
+$videoGrid = new VideoGrid($con, $userLoggedInObj);
+?>
+<div class="largeVideoGridContainer">
+
+    <?php
+
+    if(sizeof($videos) > 0) {
+        echo $videoGrid->createLarge($videos, sizeof($videos) . " results found", true);
+    }
+    else {
+        echo "No results found";
+    }
+
+    ?>
+
+</div>
 
 
 
