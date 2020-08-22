48c08a16bca0769ce4d1ed23482d2538be0b7f2a
edynak-demo <edynakdemo@gmail.com>
2020-08-22 11:11:05 -0400

    implemented video grid item test

--- watch.php
+++ watch.php
@@ -33,11 +33,15 @@ $video->incrementViews();
 </div>
 
 <div class="suggestions">
-
+    <?php
+    $videoGrid = new VideoGrid($con, $userLoggedInObj);
+    echo $videoGrid->create(null, null, false);
+    ?>
 </div>
 
 
 
 
 <?php require_once("includes/footer.php"); ?>
+                
                 
